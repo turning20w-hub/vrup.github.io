77ed26effab01f819f464e87f9588c7d204b2920
April <turning20w@gmail.com>
2026-06-04 08:50:55 +0530

    wander.js

---
+++ wander.js
@@ -0,0 +1,21 @@
+const wander = {
+  // Other Wander consoles that visitors can reach from my console.
+  consoles: [
+    'https://susam.net/wander/',
+  ],
+
+  // My favourite websites and pages I recommend to the Wander community.
+  pages: [
+    'https://susam.net/',
+    'https://susam.net/jokes.html',
+  ],
+
+  // Websites and consoles to ignore.  My console will never fetch
+  // consoles or web pages whose URLs match the following patterns.
+  ignore: [
+    // Fake examples, please replace with actual URLs.
+    'https://cookiepester.example/',
+    'https://humblebrags.example/',
+    'https://*.midreadpopup.example/',
+  ],
+}
